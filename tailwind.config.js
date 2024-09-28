234a418e2d59c66059333d2831165b361887437f
kurt-ds <delos.santos.kurt.allen@gmail.com>
2024-09-28 23:17:15 +0800

    initialized tailwind and react

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.js",
+    "./src/App.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
